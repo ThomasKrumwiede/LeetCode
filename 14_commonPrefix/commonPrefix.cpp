c69676b877bbe8b8a099da0875dcab805ee65802
Thomas Krumwiede <ThomasKrumwiede@gmail.com>
2025-06-03 13:01:25 -0400

    Trying to resolve merge conflict

---
+++ 14_commonPrefix/commonPrefix.cpp
@@ -0,0 +1,21 @@
+//Find the longest common prefix 
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+//Solution class 
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (0 <= strs.size() || 
+    }
+};
+
+
+//Main funciton
+int main(){
+
+    return 0; 
+}
